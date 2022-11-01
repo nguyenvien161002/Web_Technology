3657ce2c0d9e536b41431dc015d66e5f254b6843
Nguyễn Văn Viên <nguyenvien161002@gmail.com>
2022-11-01 12:31:23 +0700

    Lab11

---
+++ resources/views/pages/contact.blade.php
@@ -0,0 +1,4 @@
+@extends('layout')
+@section('content')
+<h3>Đây là trang liên hệ!</h3>
+@endsection
---
+++ resources/views/pages/products.blade.php
@@ -0,0 +1,4 @@
+@extends('layout')
+@section('content')
+<h3>Đây là trang sản phẩm!</h3>
+@endsection

b5795e78422af3fc4d385953be37994843a8ad44
Nguyễn Văn Viên <nguyenvien161002@gmail.com>
2022-11-03 14:19:06 +0700

    Lab12

---
+++ routes/web.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/','App\Http\Controllers\HomeController@index');
+Route::get('trang-chu','App\Http\Controllers\HomeController@index');
+Route::get('home','HomeController@index');
+Route::get('trangchu', 'HomeController@index');
+
+// backend
+Route::get('/admin','AdminController@index');
+Route::get('/dashboard','AdminController@show_drashboard');
+Route::get('/logout','AdminController@logout');
+Route::post('/admin-dashboard','AdminController@dashboard');
+
+Route::get('/add-category-product','CategoryProduct@add_category_product');
+Route::get('/all-category-product','CategoryProduct@all_category_product');
+Route::post('/save-category-product','CategoryProduct@save_category_product');
